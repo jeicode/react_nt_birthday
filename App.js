5439fd9ce3f7846d0321ae9ffdc224da6bbe1287
jeiobaco <jeison.obando@paxzu.co>
2023-01-20 19:09:47 -0500

    layout auth

--- App.js
+++ App.js
@@ -1,21 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet } from 'react-native';
+import Auth from './src/components/Auth';
+import { AuthContextProvider } from './src/context/AuthContext';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar style="light" />
+      <SafeAreaView style={styles.background}>
+        <AuthContextProvider>
+          <Auth />
+        </AuthContextProvider>
+      </SafeAreaView>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  background: {
+    backgroundColor: '#15212b',
+    height: '100%',
   },
 });
